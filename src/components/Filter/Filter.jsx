b30a4867db047420b681569a668e3d10185fb23b
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-10-17 09:56:58 +0300

    edited Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,34 +1,21 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { SpanName, LabelContainer, Input } from './Filter.styled';
 
-export class Filter extends Component {
-  state = {
-    filter: '',
-  };
-
-  changeFilter = e => {
-    const { value } = e.target;
-    this.setState({ filter: value });
-
-    this.props.onChangeFilter(value);
-  };
-
-  render() {
-    return (
-      <LabelContainer>
-        <SpanName>Find contacts by name</SpanName>
-        <Input
-          type="text"
-          name="filter"
-          value={this.state.filter}
-          onChange={this.changeFilter}
-        />
-      </LabelContainer>
-    );
-  }
-}
+export const Filter = ({ filter, onChangeFilter }) => {
+  return (
+    <LabelContainer>
+      <SpanName>Find contacts by name</SpanName>
+      <Input
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={e => onChangeFilter(e.target.value)}
+      />
+    </LabelContainer>
+  );
+};
 
 Filter.propTypes = {
+  filter: PropTypes.string,
   onChangeFilter: PropTypes.func.isRequired,
 };
